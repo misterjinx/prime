ad6a80dca68e8f6070d273228eb14f2ed4b7d4e1
Marius <marius@freshcolors.ro>
2015-08-15 17:56:16 +0300

    Update Literal route teste to better test getMatches

--- tests/Router/Route/LiteralTest.php
+++ tests/Router/Route/LiteralTest.php
@@ -6,6 +6,18 @@ use Prime\Router\Route\Literal;
 
 class LiteralTest extends \PHPUnit_Framework_TestCase
 {
+    public function testInstantiation()
+    {
+        $route = new Literal('/foo');
+        $this->assertInstanceOf('Prime\Router\Route\Literal', $route);
+    }
+
+    public function testConstructorSetsDefaultsIfProvided()
+    {
+        $route = new Literal('/foo', array('bar' => 'baz'));
+        $this->assertSame(array('bar' => 'baz'), $route->getDefaults());
+    }
+
     public function testMatch()
     {
         $route = new Literal('/test', array(
@@ -16,6 +28,31 @@ class LiteralTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($route->match('/test'));
         $this->assertFalse($route->match('/tests'));
     }
+
+    public function testMatchGetMatches()
+    {
+        $route = new Literal('/', array(
+            'controller' => 'foo'
+        ));
+
+        $this->assertTrue($route->match('/'));
+        $this->assertSame('foo', $route->getMatch('controller'));
+        $this->assertFalse($route->getMatch('action'));
+        $this->assertSame(array('controller' => 'foo'), $route->getMatches());
+    }
+
+    public function testMatchesReturnsDefaults()
+    {
+        $route = new Literal('/foo', array(
+            'controller' => 'foo'
+        ));
+
+        $this->assertTrue($route->match('/foo'));
+
+        $defaults = $route->getDefaults();
+        $matches = $route->getMatches();
+        $this->assertSame($defaults, $matches);
+    }    
 
     public function testAssemble()
     {
