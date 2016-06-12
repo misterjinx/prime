1f8c4aa96d4589798ff34bc9bfa70348dd403f35
Marius <marius@freshcolors.ro>
2016-06-12 14:26:49 +0300

    Add a default route and missing error view file to demo

--- demo/public/index.php
+++ demo/public/index.php
@@ -2,6 +2,11 @@
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
+$router = new \Prime\Router(true);
+$router->add('home', new \Prime\Router\Route\Literal('/', array(
+    'controller' => 'index'
+)));
+
 $dispatcher = new \Prime\Dispatcher\ControllerActionDispatcher(
     new \Prime\Controller\ControllerActionResolver(true, 'Demo\Controller')
 );
@@ -10,6 +15,7 @@ $view = new \Prime\View(new \Prime\View\Engine\PhpEngine(
 ));
 
 $container = new \Prime\Container();
+$container->set('router', $router);
 $container->set('dispatcher', $dispatcher);
 $container->set('view', $view);
 
